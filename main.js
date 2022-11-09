c0a7043e40971c14f4af4909df7b8f8a1e8fe894
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2022-11-09 11:44:15 +0100

    ChangeName-Btns+innerhtml

--- main.js
+++ main.js
@@ -6,40 +6,41 @@ let removeBtn = document.getElementById("removeAll");
 let li = document.createElement("li");
 
 btn.addEventListener("click", function () {
-
   let name = inputName.value;
   let phone = inputPhone.value;
 
   let li = document.createElement("li");
   let nameList = document.createElement("input");
   let phoneList = document.createElement("input");
-  let modify = document.createElement("button");
-  let remove = document.createElement("button");
+  let modifyBtn = document.createElement("button");
+  let removeBtn = document.createElement("button");
 
   nameList.setAttribute("disabled", "");
   phoneList.setAttribute("disabled", "");
 
-  modify.innerHTML = "Ändra";
-  remove.innerHTML = "Radera";
+  modifyBtn.innerHTML = "Ändra";
+  removeBtn.innerHTML = "Radera";
 
   nameList.value = inputName.value;
   phoneList.value = inputPhone.value;
 
-  li.append(nameList, phoneList, modify, remove);
+  li.append(nameList, phoneList, modifyBtn, removeBtn);
   ul.append(li);
-/// MODIFY BUTTON CLICK FUNCTION
-  modify.addEventListener("click", function () {
+  /// MODIFY BUTTON CLICK FUNCTION
+  modifyBtn.addEventListener("click", function () {
     let selectedParentElement = this.parentNode;
     console.log(selectedParentElement);
     if (nameList.disabled && phoneList.disabled) {
       nameList.removeAttribute("disabled", "");
       phoneList.removeAttribute("disabled", "");
+      modifyBtn.innerHTML = "Spara";
     } else {
       nameList.setAttribute("disabled", "");
       phoneList.setAttribute("disabled", "");
+      modifyBtn.innerHTML = "Ändra";
     }
   });
-  remove.addEventListener("click", function () {
+  removeBtn.addEventListener("click", function () {
     let selectedParentElement = this.parentNode;
     selectedParentElement.remove();
   });
@@ -65,4 +66,3 @@ removeBtn.addEventListener("click", function () {
     ul.removeChild(ul.firstChild);
   }
 });
-
