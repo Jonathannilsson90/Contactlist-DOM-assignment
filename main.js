cb701783b2645412a0bbcebc1e5796a1b9b54d0b
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2022-11-09 17:26:46 +0100

    finishing-touches-comments-and-so-on

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
+///DECLARING VARIABLES TO CATCH CERTAIN ID'S
+
 let inputName = document.getElementById("namn-input");
 let inputPhone = document.getElementById("tlf-input");
 let btn = document.getElementById("create-Btn");
 let ul = document.getElementById("ul-list");
 let removeBtn = document.getElementById("removeAll");
-let li = document.createElement("li");
+
+/// CREATE-BUTTON EVENTLISTENER
 
 btn.addEventListener("click", function () {
   let name = inputName.value;
   let phone = inputPhone.value;
+
+  /// WHEN BUTTON IS PRESSED CREATE FOLLOWING ELEMENTS:
 
   let li = document.createElement("li");
   let nameList = document.createElement("input");
   let phoneList = document.createElement("input");
   let modifyBtn = document.createElement("button");
   let removeBtn = document.createElement("button");
+
+  /// DECLARE THESE ATTRIBUTES TO THE TWO INPUT-FIELDS:
 
   nameList.setAttribute("disabled", "");
   phoneList.setAttribute("disabled", "");
@@ -24,9 +31,13 @@ btn.addEventListener("click", function () {
   nameList.value = inputName.value;
   phoneList.value = inputPhone.value;
 
+  ///APPEND "MAKE THE ELEMENTS ALIVE" THAT WE CREATED ABOVE:
+
   li.append(nameList, phoneList, modifyBtn, removeBtn);
   ul.append(li);
-  /// MODIFY BUTTON CLICK FUNCTION
+
+  /// MODIFY-BUTTON
+
   modifyBtn.addEventListener("click", function () {
     let selectedParentElement = this.parentNode;
     if (nameList.disabled && phoneList.disabled) {
@@ -39,13 +50,15 @@ btn.addEventListener("click", function () {
       modifyBtn.innerHTML = "Ändra";
     }
   });
+  /// REMOVE-BUTTON
+
   removeBtn.addEventListener("click", function () {
     let selectedParentElement = this.parentNode;
     selectedParentElement.remove();
   });
 });
 
-///CLEAR INPUT FIELDS FROM TEXT
+///CLEAR INPUT-FIELDS FROM TEXT
 
 let createButton = document.getElementById("create-Btn");
 btn.addEventListener("click", function (e) {
